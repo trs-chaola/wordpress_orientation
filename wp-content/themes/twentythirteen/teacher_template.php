6a12a61cbb984c613ab284c3a57c03bc6a86457e
trd-vergel <trd.vergel@gmail.com>
2014-03-12 19:15:00 +0800

    create teachers page

---
+++ wp-content/themes/twentythirteen/teacher_template.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Template Name: Teacher page
+*/
+
+ ;?>
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/teacher_layout.css" />
+		<title>
+			Teachers page
+		</title>
+	</head>
+	<body>
+		<div class="teacher_container">
+			<div class="teacher_header_wrap border">
+				<div class="t_lefthead_text border">
+					初級者から上級者までレベルに合わせた学習プログラム
+				</div>
+				<div class="t_righthead_text border">
+					よくあるご質問 | お問い合わせ			
+				</div>
+			</div>
+			<div class="teacher_body">
+				<div class="teacher_dbelogo border">
+					<img alt="DBE LOGO" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/DBE_logo.png" />
+				</div>
+				<div class="teacher_navigation border">
+					<ul class="t_nav_ul">
+						<li><img alt="" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/menu/menu01/news_menu.png" /></li>
+						<li><img alt="" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/menu/menu01/features_menu.png" /></li>
+						<li><img alt="" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/menu/menu01/curriculum_menu.png" /></li>
+						<li><img alt="" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/menu/menu01/student_life_menu.png" /></li>
+						<li><img alt="" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/menu/menu01/accommodation_menu.png" /></li>
+						<li><img alt="" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/menu/menu01/how_to_play_menu.png" /></li>
+					</ul>
+				</div>
+					<br/>
+				<div class="t_body_head">
+					<div class="teach_head">
+						<img style="float:left" alt="TEACHERS" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/teachers_icon.png" />
+						<span style="margin-left: 10px;">Teachers<br/>&nbsp;&nbsp;&nbsp;講師紹介</span>
+						<span style="float:right">TOP > 講師紹介</span>
+					</div>
+					<div class="teacher_intro">
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ドリーム・ビッグ・イングリッシュは全員ネイティブスピーカーの洗練された講師を取り揃えております。<br/>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;生徒のレベルに合わせ多様なクラスで丁寧に指導いたします。
+					</div>
+					<div class="teachers_statement">
+						<div  style="float:left"class="t_mary">
+							<img alt="TEACHERS" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/mary_pic.png" height="250px" />
+							<p>
+								MARY先生<br/>
+								講師として、生徒一人一人を理解し、彼らの英語学習を支えるためにもその違いを考慮しなければいけないと考えています。
+							</p>
+						</div>
+						<div style="float:right" class="t_mary">
+							<img alt="TEACHERS" src="<?php echo get_template_directory_uri(); ?>/teacher_page/img/mary_pic.png" height="250px" />
+							<p>
+								MARY先生<br/>
+								講師として、生徒一人一人を理解し、彼らの英語学習を支えるためにもその違いを考慮しなければいけないと考えています。
+							</p>
+						</div>
+						<div></div>
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
