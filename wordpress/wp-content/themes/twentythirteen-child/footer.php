0c96d0079f8bec2c69276ffd1bb8c6e48684abc6
trd-isaac <trd.isaac@gmail.com>
2014-03-13 18:02:39 +0800

    transfer all files to wordpress folder

---
+++ wordpress/wp-content/themes/twentythirteen-child/footer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<div class="pre-footer">
+			<div class="left">
+				<div class="company-info">
+					<img src="<?php echo get_template_directory_uri(); ?>-child/images/header_and_footer/DBE_logo_footer.png"/>
+					<p class="company-address">
+						no47A VISAYAS ST MARTIN VILLAGE A.S.FORTUNA ST MANDAUE CITY CEBU 6014
+					</p>
+					<img src="<?php echo get_template_directory_uri(); ?>-child/images/header_and_footer/tel_number.png"/>
+					<p>
+						受付時間 08:00~22:00 （月～金曜日）<br>
+						※土日、祝日は休み
+					</p>
+				</div>
+			</div>
+			<div class="left">
+				<div class="company-links">
+					<ul>
+						<li><a href="#" class="company-links-entry">当校の特徴</a></li>
+						<li><a href="#" class="company-links-entry">カリキュラム</a></li>
+						<li><a href="#" class="company-links-entry">カレンダー</a></li>
+						<li><a href="#" class="company-links-entry">講師紹介</a></li>
+						<li><a href="#" class="company-links-entry">宿泊施設</a></li>
+						<li><a href="#" class="company-links-entry">Ｑ＆Ａ</a></li>
+						<li><a href="#" class="company-links-entry">生徒の生活環境</a></li>
+						<li><a href="#" class="company-links-entry">応募方法</a></li>
+						<li><a href="#" class="company-links-entry">お問い合わせ</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="left">
+				<div class="company-map">
+					<img src="<?php echo get_template_directory_uri(); ?>-child/images/header_and_footer/dbe_map.png"/>
+				</div>
+			</div>
+			<br style="clear:both;"/>
+		</div>
+		<div class="footer-spacer"> </div>
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			
+			<div class="site-info">
+				<img src="<?php echo get_template_directory_uri(); ?>-child/images/header_and_footer/dbe_footer-Copy.png"/><br>
+				<span>Copyright &copy; Dream Big English. All rights reserved</span>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+</body>
+</html>
---
+++ wordpress/wp-content/themes/twentythirteen-child/header.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+	<script type="text/javascript">
+	 document.createElement('header');
+	 document.createElement('nav');
+	 document.createElement('menu');
+	 document.createElement('section');
+	 document.createElement('article');
+	 document.createElement('aside');
+	 document.createElement('footer');
+	</script>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</a>
+			<div class="top-content">
+				<div class="top-content-text">
+					<p class="left">
+						初級者から上級者までレベルに合わせた学習プログラム
+					</p>
+					<p class="right">
+						<a href="#">よくあるご質問</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">お問い合わせ</a>
+					</p>
+				</div>
+			</div>
+			<div class="header-logo">
+				<a href="#">
+					<img src="<?php echo get_template_directory_uri(); ?>-child/images/header_and_footer/DBE_logo.png" alt="Dream Big English Logo"/>
+				</a>
+			</div>
+			
+			<div id="navbar" class="navbar">
+				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
+					<div class="page_item_news left" onclick="location.href='#';"></div>
+					<div class="page_item_features left" onclick="location.href='#';"></div>
+					<div class="page_item_curriculum left" onclick="location.href='#';"></div>
+					<div class="page_item_student_life left" onclick="location.href='#';"></div>
+					<div class="page_item_accomodation left" onclick="location.href='http://wordpress-orientation.localhost/?page_id=4';"></div>
+					<div class="page_item_apply left" onclick="location.href='#';"></div>
+				</nav><!-- #site-navigation -->
+			</div><!-- #navbar -->
+		</header><!-- #masthead -->
+
+		<div id="main" class="site-main">
